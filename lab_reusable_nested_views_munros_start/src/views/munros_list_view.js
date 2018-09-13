9424b58f0171a75b7c8efc11c1af08e332b62260
Fofiko <foteinid@gmail>
2018-09-13 14:22:57 +0100

    Add display for all munros

--- lab_reusable_nested_views_munros_start/src/views/munros_list_view.js
+++ lab_reusable_nested_views_munros_start/src/views/munros_list_view.js
@@ -6,10 +6,27 @@ const MunrosListView = function (container) {
 
 MunrosListView.prototype.bindEvents = function () {
   PubSub.subscribe("Munros:munros-data-ready", (evt) => {
-    this.munros = evt.detail;
-
 // console.log("got the data in list view", this.munros);
-  })
+    this.container.innerHTML = "";
+    // munros = evt.detail
+    const munros = evt.detail;
+    // munros.forEach
+    munros.forEach((munro) => {
+      console.log(munro.name);
+      this.render("h1", munro.name);
+      this.render("h3", munro.meaning);
+      this.render("h3", munro.height);
+    })
+
+  });
 };
+
+MunrosListView.prototype.render = function (tag, element_name) {
+  // console.log(element_name);
+  const item = document.createElement(tag);
+  item.textContent = element_name;
+  this.container.appendChild(item);
+};
+
 
 module.exports = MunrosListView;
